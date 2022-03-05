90ff118eca1ce48dd6ebd0b6e3444b4d23f95d4c
Arden Ma <ardenma@stanford.edu>
2022-03-04 23:31:55 -0800

    added weighted majority voter model

--- src/models/voting.py
+++ src/models/voting.py
@@ -14,9 +14,25 @@ class MajorityVoter:
         vote_counts = list(vote_counter.values())
         max_votes = max(vote_counts)
 
+        # Randomly break ties
         candidates = []
         for i in range(len(vote_counts)):
             if vote_counts[i] == max_votes:
                 candidates.append(labels[i])
         
         return np.random.choice(candidates)
+
+class WeightedMajorityVoter:
+
+    def __call__(self, batch_votes: List[List[int]], batch_similarities: List[List[float]]) -> List[int]:
+        return [self.vote_func(votes, similarities) for votes, similarities in zip(batch_votes, batch_similarities)]
+    
+    def vote_func(self, votes: List[int], similarities: List[float]) -> int:
+        scores = {}
+        for vote, similarity in zip(votes, similarities):
+            if vote not in scores:
+                scores[vote] = similarity
+            else:
+                scores[vote] += similarities
+        
+        return max(scores, key=scores.get)
