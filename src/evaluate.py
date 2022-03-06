42e49d36028e342e03f6e9e5bdb4b46b3caeecd5
Arden Ma <ardenma@stanford.edu>
2022-03-05 17:35:24 -0800

    updated evaluate.py to give option to evaluate with softmax head

--- src/evaluate.py
+++ src/evaluate.py
@@ -11,6 +11,7 @@ from sklearn.metrics import accuracy_score, f1_score
 
 from models.distilbert import DistilBertForSequenceEmbedding
 from models.voting import WeightedMajorityVoter
+from models.heads import SoftmaxHead
 from utils.data import LiarDataset
 from utils.index import cache_index
 from utils.artifacts import get_model_path_from_artifact
@@ -27,19 +28,19 @@ artifacts_dir = os.path.join(cwd, "artifacts")
 if not os.path.exists(artifacts_dir): os.mkdir(artifacts_dir)
 
 
-def eval_wrapper(args):
+def eval_contrastive_wrapper(args):
   test_dataset = LiarDataset("test", num_labels=num_labels)
   test_ldr = DataLoader(test_dataset, batch_size=10)
   id_map = LiarDataset("train", num_labels=num_labels).get_id_map()
 
   logging.info("Loading models...")
   embedding_model = DistilBertForSequenceEmbedding()
-  if args.model_path:
-    embedding_model.load(args.model_path)
+  if args.embedding_model_path:
+    embedding_model.load(args.embedding_model_path)
     if args.index_path:
       index = faiss.read_index(args.index_path)
     else:
-      model_name = os.path.basename(args.model_path).split('.')[0]
+      model_name = os.path.basename(args.embedding_model_path).split('.')[0]
       index = cache_index(model_name, embedding_model, num_labels)
 
   elif args.artifact:  # e.g. daily-tree-15-3-labels:v4
@@ -55,7 +56,19 @@ def eval_wrapper(args):
 
   logging.info(f"Running evaluation with model: '{model_name}'...")
   eval_contrastive(embedding_model, index, prediction_model, K, id_map, test_ldr)
-  
+
+def eval_softmax_wrapper(args):
+  test_dataset = LiarDataset("test", num_labels=num_labels)
+  test_ldr = DataLoader(test_dataset, batch_size=10)
+  embedding_model = DistilBertForSequenceEmbedding()  
+  embedding_model.load(args.embedding_model_path)
+
+  prediction_model = SoftmaxHead(input_length=embedding_model.embedding_size, num_classes=num_labels)
+  prediction_model.load(args.prediction_model_path)
+
+  eval_softmax(embedding_model, prediction_model, test_ldr)
+
+
 def eval_contrastive(embedding_model: DistilBertForSequenceEmbedding, index: faiss.IndexIDMap, prediction_model: WeightedMajorityVoter, K: int, id_map, dataloader: DataLoader):
   if torch.cuda.is_available():
     logging.info("GPU available!")
@@ -88,20 +101,57 @@ def eval_contrastive(embedding_model: DistilBertForSequenceEmbedding, index: fai
 
   metrics = {"accuracy": accuracy_score(labels, predictions), "f1_score": f1_score(labels, predictions, average='weighted')}
 
-  logging.info(f"Eval accuracy: {metrics['accuracy']}")
-  logging.info(f"Eval weighted f1_score: {metrics['f1_score']}")
+  logging.info(f"Eval (contrastive) accuracy: {metrics['accuracy']}")
+  logging.info(f"Eval (contrastive) weighted f1_score: {metrics['f1_score']}")
+
+  return metrics
+
+def eval_softmax(embedding_model: DistilBertForSequenceEmbedding, prediction_model: SoftmaxHead, dataloader: DataLoader):
+  if torch.cuda.is_available():
+    logging.info("GPU available!")
+    embedding_model.to('cuda')
+    prediction_model.to('cuda')
+  
+  logging.info("Staring evaluation...")
+  predictions = []
+  labels = []
+  with torch.no_grad():
+    for (batch_idx, batch) in tqdm(enumerate(dataloader)):
+      # Generate embeddings
+      embeddings = embedding_model(batch["data"]) 
+      logits = prediction_model(embeddings)
+      y_pred = torch.argmax(logits, axis=-1)
+      y_label = batch["label"]
+
+      # Update list of predictions and labels
+      for pred in y_pred:
+        predictions.append(int(pred))
+      for label in y_label:
+        labels.append(int(label))
+    
+  for label, pred in zip(labels, predictions):
+    logging.info(f"model predicted {pred} with label {label}")
+
+  metrics = {"accuracy": accuracy_score(labels, predictions), "f1_score": f1_score(labels, predictions, average='weighted')}
+
+  logging.info(f"Eval (softmax) accuracy: {metrics['accuracy']}")
+  logging.info(f"Eval (softmax) weighted f1_score: {metrics['f1_score']}")
 
   return metrics
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Evaluate a model.')
-    parser.add_argument('--model_path', type=str)
+    parser.add_argument('--embedding_model_path', type=str)
+    parser.add_argument('--prediction_model_path', type=str)
     parser.add_argument('--index_path', type=str)
     parser.add_argument('--artifact', type=str)
+    parser.add_argument('--contrastive', action="store_true")
     args = parser.parse_args()
 
-    if not args.model_path and not args.artifact:
-        raise Exception("Need to specify a model path or artifact!")
-
-    eval_wrapper(args)
+    if args.contrastive and (args.embedding_model_path or args.artifact):
+      eval_contrastive_wrapper(args)
+    elif args.embedding_model_path and args.prediction_model_path:
+      eval_softmax_wrapper(args)
+    else:
+      raise Exception("Invalid configuration options.")
